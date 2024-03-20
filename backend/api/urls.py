f5d717c600db4673ee08fdf9e14c6aafb6f713ab
Ayush Bhattarai <ayushbhattaraiofficial@gmail.com>
2024-03-20 07:35:30 +0545

    Set urls for login api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from django.conf import settings
-from django.conf.urls.static import static
 from .views import (
     DemographicsList,
     CircumstancesList,
@@ -45,5 +44,6 @@ router.register(r'classification_result', ClassificationResultList, basename='cl
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/login', obtain_auth_token, name='api_login')
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
